1e0c2e597e71b2fc0103287675130bb43171fd47
kdai11830 <kdai@college.harvard.edu>
2020-03-26 19:00:09 -0700

    Update psa-script.sh

--- scripts/psa-script.sh
+++ scripts/psa-script.sh
@@ -2,14 +2,14 @@
 #SBATCH -n 1
 #SBATCH -N 1
 #SBATCH -t 0-04:00
-#SBATCH --mem=60000
+#SBATCH --mem=10000
 #SBATCH -o /n/home13/kdai/multi-context-gen/output_%j.out
 #SBATCH -e /n/home13/kdai/multi-context-gen/errors_%j.err
 #SBATCH -p gpu
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --gpu-freq=high
 
 module load cuda/9.0-fasrc02
 module load Anaconda3/5.0.1-fasrc01
 source activate myenv
-python ./encoder-agnostic-model/train.py -config ./encoder-agnostic-model/config/ac/transformer_ac_psa.yml -save_config ./encoder-agnostic-model/config/ac/transformer_ac_psa2.yml -run_name psa -gpt2_params_path ./encoder-agnostic-model/gpt2/models/124M/ -gpt2_init_embanddec
+python ./encoder-agnostic-model/train.py -config ./encoder-agnostic-model/config/ac/transformer_ac_psa.yml -run_name psa -gpt2_params_path ./encoder-agnostic-model/gpt2/models/124M/ -gpt2_init_embanddec
